31739a68a6dc8ca20b4f6461cfdc1fc9f412d90e
Stanislav Linnik <404notfoundhard@gmain.com>
2019-09-11 12:35:54 +0700

    release 0.1.2

--- flask/app/config.py
+++ flask/app/config.py
@@ -1,4 +1,5 @@
 class Configuration(object):
-    DEBUG = True
+    DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:root@192.168.99.100:5432/my_bd'
+    SECRET_KEY = b'?om(quV\q]E:}7gh|@kJ/XzAfs@crdKHp"|dqTFU'
