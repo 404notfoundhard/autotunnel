2fe9d6d99b4c7f5bd0f50bc47f2a9701729d0ad3
Stanislav Linnik <404notfoundhard@gmain.com>
2019-09-16 13:03:34 +0700

    update gitignore, untrack test files

--- api-client/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-token = {'token': '12345'}
-
-while True:
-    r = requests.post('http://localhost:9999/get_info/test', data=token)
-    print(r.json()['ssh_port'], r.json()['db_port'], r.json()['vnc_port'])
-    time.sleep(5)
