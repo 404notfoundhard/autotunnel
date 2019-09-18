92ccce3d6e8d2a3fc47fdc07a89c53329f705666
Stanislav Linnik <404notfoundhard@gmain.com>
2019-09-18 18:21:53 +0700

    refac(web,api-s): getting sensitive data through environment variables

--- web-server/app/config.py
+++ web-server/app/config.py
@@ -1,7 +1,16 @@
+from os import environ
+
+
 class Configuration(object):
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://postgres:root@192.168.99.100:5432/my_bd'
-    SECRET_KEY = b'?om(quV\q]E:}7gh|@kJ/XzAfs@crdKHp"|dqTFU'
+    SQLALCHEMY_DATABASE_URI = ('postgres+psycopg2://'
+                               + environ['POSTGRES_LOGIN'] + ':'
+                               + environ['POSTGRES_PASSWORD'] + '@'
+                               + environ['POSTGRES_ADDRESS'] + ':'
+                               + environ['POSTGERS_PORT'] + '/'
+                               + environ['POSTGRES_DATABASE'])
+
+    SECRET_KEY = environ['SECRET_KEY'].encode('utf-8')
     # SERVER_NAME = '0.0.0.0:9991'
     # PORT = '9991'
